996db55c7af2375fa2ae6c338546c1b4bbb908e4
Donatas Glodenis <dgvirtual@akl.lt>
2023-05-02 19:21:23 +0300

    make table responsive (Bootstrap table-responsive)

--- Pages/Views/_table.php
+++ Pages/Views/_table.php
@@ -1,20 +1,22 @@
-<table class="table table-hover">
-    <?= $this->include('_table_head') ?>
-    <tbody>
-    <?php if (isset($pages) && count($pages)) : ?>
-        <?php foreach ($pages as $page) : ?>
-            <tr>
-                <?php if (auth()->user()->can('pages.delete')): ?>
-                    <td>
-                        <input type="checkbox" name="selects[<?= $page->id ?>]" class="form-check">
-                    </td>
-                <?php endif ?>
-                <?= view('App\Modules\Pages\Views\_row_info', ['page' => $page]) ?>
-            </tr>
-        <?php endforeach ?>
-    <?php endif ?>
-    </tbody>
-</table>
+<div class="table-responsive">
+    <table class="table table-hover">
+        <?= $this->include('_table_head') ?>
+        <tbody>
+        <?php if (isset($pages) && count($pages)) : ?>
+            <?php foreach ($pages as $page) : ?>
+                <tr>
+                    <?php if (auth()->user()->can('pages.delete')): ?>
+                        <td>
+                            <input type="checkbox" name="selects[<?= $page->id ?>]" class="form-check">
+                        </td>
+                    <?php endif ?>
+                    <?= view('App\Modules\Pages\Views\_row_info', ['page' => $page]) ?>
+                </tr>
+            <?php endforeach ?>
+        <?php endif ?>
+        </tbody>
+    </table>
+</div>
 
 <?php if (auth()->user()->can('pages.delete')) : ?>
     <input type="submit" value="<?= lang('Pages.deleteSelected') ?>" class="btn btn-sm btn-outline-danger" />
