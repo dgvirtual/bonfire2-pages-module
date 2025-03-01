3879fea561acdebadf4ccfeefc6cc6defdc9ab79
Donatas Glodenis <dgvirtual@akl.lt>
2025-03-01 17:15:11 +0200

    Prevent unnecessary widget queries
    related to https://github.com/lonnieezell/Bonfire2/pull/564

--- Pages/Module.php
+++ Pages/Module.php
@@ -2,10 +2,9 @@
 
 namespace App\Modules\Pages;
 
+use App\Modules\Pages\Models\PagesModel;
 use Bonfire\Core\BaseModule;
 use Bonfire\Menus\MenuItem;
-
-use App\Modules\Pages\Models\PagesModel;
 use Bonfire\Widgets\Types\Charts\ChartsItem;
 use Bonfire\Widgets\Types\Stats\StatsItem;
 
@@ -16,6 +15,9 @@ class Module extends BaseModule
      */
     public function initAdmin()
     {
+        // true if we are on Dashboard page
+        $isDashboard = current_url() === config('App')->baseURL . '/' . ADMIN_AREA;
+
         // Add to the Content menu
         $sidebar = service('menus');
         $item    = new MenuItem([
@@ -26,23 +28,41 @@ class Module extends BaseModule
         ]);
         $sidebar->menu('sidebar')->collection('content')->addItem($item);
 
-        $widgets = service('widgets');
-        $pages = new PagesModel();
+        $widgets   = service('widgets');
+        $pages     = new PagesModel();
+        $itemId0    = 'pagesCount2347';
         $statsItem = new StatsItem([
             'bgColor' => 'bg-blue',
-            'title' => lang('Pages.pageTitle'),
-            'value' => $pages->where('deleted_at', null)->countAllResults(),
-            'url' => ADMIN_AREA . '/pages',
-            'faIcon' => 'fas fa-file',
+            'title'   => lang('Pages.pageTitle'),
+            'id'      => 'pagesCount2347',
+            'value'   => (setting('Stats.Stats_' . $itemId0) === 'on' && $isDashboard) ? $pages->where('deleted_at', null)->countAllResults() : null,
+            'url'     => ADMIN_AREA . '/pages',
+            'faIcon'  => 'fas fa-file',
         ]);
-        $widgets->widget("stats")->collection('stats')->addItem($statsItem);
+        $widgets->widget('stats')->collection('stats')->addItem($statsItem);
 
+        $itemId    = 'pagesByCategory129';
         $statsItem = new ChartsItem([
-            'title'   => lang('Pages.pagesClassByCat'),
-            'type'   => 'pie',
-            'cssClass'   => 'col-3',
+            'title'    => lang('Pages.pagesClassByCat'),
+            'type'     => 'pie',
+            'id'       => $itemId,
+            'cssClass' => 'col-3',
         ]);
-        $statsItem->addDataset('pages', 'category', 'id');
+        if (setting('Stats.Charts_' . $itemId) === 'on' && $isDashboard) {
+            $statsItem->addDataset('pages', 'category', 'id');
+        }
         $widgets->widget('charts')->collection('charts')->addItem($statsItem);
+
+        $itemId2    = 'pagesByCategory435';
+        $statsItem2 = new ChartsItem([
+            'title'    => lang('Pages.pagesClassByCat'),
+            'type'     => 'bar',
+            'id'       => $itemId2,
+            'cssClass' => 'col-6',
+        ]);
+        if (setting('Stats.Charts_' . $itemId2) === 'on' && $isDashboard) {
+            $statsItem2->addDataset('pages', 'category', 'id');
+        }
+        $widgets->widget('charts')->collection('charts')->addItem($statsItem2);
     }
 }
